dbd9b0c1eb697c3143c573efcf3de92ec4ba481d
snehalgupta <snehal16201@iiitd.ac.in>
2018-03-29 07:27:58 +0530

    update test2

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ task=0
 
 stack=[]
 variables=[]
-operator={"plus":"+","minus":"-","into":"*","divide":"/","mod":"%","or":"or","and":"and","not":"!"}
+operator={"plus":"+","minus":"-","into":"*","divided":"/","divide":"/","mod":"%","or":"or","and":"and","not":"!","gt":">","lt":"<","gte":">=","lte":"<=","eq":"==","ne":"!="}
 blocks=["if","while","for"]
 
 class token :
@@ -11,6 +11,8 @@ class token :
      def __init__(self,s,l):
          self.token = s
          self.req = l
+
+cmd = "create for loop of variable i from 1 to 10"
 
 while True:
     sent = cmd.lower().split(" ")
@@ -39,15 +41,18 @@ while True:
             task=2
         elif i=="function":
             task=3
-            stack.aapend(token("print",[]))
+            stack.append(token("print",[]))
+            nextvar=1
         elif task==1:
-            if i=="variable":
-                nextassignee=1
             if nextassignee==1:
                 nextassignee=0
                 stack.append(token("var",i))
-            elif i=="to":
+            elif i=="with":
                 assignedflag=1
+
+            if i=="variable" and assignedflag==0:
+                nextassignee=1
+            
             if assignedflag==1:
                 
                 if nextval==1:
@@ -55,7 +60,7 @@ while True:
                     nextval=0
                 elif i in operator:
                     assigned+=operator[i]
-                elif type(i)==int or type(i)==float:
+                elif i.isdigit() :
                     assigned+=str(i)
 
                 if i=="variable":
@@ -64,7 +69,8 @@ while True:
                     temp=stack[-1].req
                     stack[-1].req=[temp,assigned]
                     assigned=""
-                    stack.append('[')
+                    assignedflag=0
+                    #stack.append('[')
         elif task==2:
             
             if subtask==1:
@@ -75,8 +81,8 @@ while True:
                     nextval=0
                     stack[-1].req=[i]
                     st=1
-                elif st==1 and (type(i)==int or type(i)==float ) :
-                    stack[-1].req.append("i")
+                elif st==1 and (i.isdigit() ) :
+                    stack[-1].req.append(str(i))
                     st=0
                     end=1
                 elif st==1 and nextval==1 :
@@ -86,8 +92,8 @@ while True:
                     nextval=0
                 elif st==1 and (i=="variable" ) :
                     nextval=1
-                elif end==1 and (type(i)==int or type(i)==float ) :
-                    stack[-1].req.append("i")
+                elif end==1 and (i.isdigit() ) :
+                    stack[-1].req.append(str(i))
                     end=0
                 elif end==1 and nextval==1 :
                     stack[-1].req.append(i)
@@ -98,30 +104,34 @@ while True:
                     nextval=1
 
             elif subtask==2:
-                if nextvar==1 and i=="variable":
-                    nextval=1
-                    nextvar=0
-                if nextval==1:
+            	if nextval==1:
                     assigned+=i
                     nextval=0
                     nextvar=1
                 elif i in operator:
-                    assigned+=operator["i"]
+                    assigned+=operator[i]
+                elif i.isdigit():
+                	assigned+=str(i)
+                if nextvar==1 and i=="variable":
+                    nextval=1
+                    nextvar=0
                 if count==(len(sent)):
                     stack[-1].req.append(assigned)
                     assigned=""
                     stack.append('[')
 
             elif subtask==3:
-                if nextvar==1 and i=="variable":
-                    nextval=1
-                    nextvar=0
-                if nextval==1:
+            	if nextval==1:
                     assigned+=i
                     nextval=0
                     nextvar=1
                 elif i in operator:
-                    assigned+=operator["i"]
+                    assigned+=operator[i]
+                elif i.isdigit():
+                	assigned+=str(i)
+                if nextvar==1 and i=="variable":
+                    nextval=1
+                    nextvar=0
                 if count==(len(sent)):
                     stack[-1].req.append(assigned)
                     assigned=""
@@ -136,22 +146,30 @@ while True:
                 elif i=="if":
                     nextvar=1
                     subtask=2
-                    stack.append(token("if",[]))
+                    #stack.append(token("if",[]))
                 elif i=="while":
                     nextvar=1
                     subtask=3
-                    stack.append(token("while",[]))
+                    #stack.append(token("while",[]))
         elif task==3:
             if nextvar==1 and i=="variable":
                 nextval=1
                 nextvar=0
-            if nextval==1:
+            elif nextval==1:
                 assigned+=i
                 nextval=0
                 nextvar=1
             elif i in operator:
-                assigned+=operator["i"]
+                assigned+=operator[i]
+            elif i.isdigit() :
+                 assigned+=str(i)
             if count==(len(sent)):
                 stack[-1].req.append(assigned)
                 assigned=""
     task=0
+    for i in stack:
+    	if i == "[" or i=="]":
+    		print (i)
+    	else:
+        	print (i.token,i.req)
+    break
