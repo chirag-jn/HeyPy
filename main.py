86d9871bc6d781585639a90b87bc82dd4d104603
Chirag Jain <jainchg@gmail.com>
2018-03-29 06:49:26 +0530

    starting with main, but why?

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-text = input().split()
+user = "b2aca159-86a0-4afa-9b22-4b720ee1957f"
+passwd = "vTSl6VKjZMPA"
 
-ans = ''
-for i in range(len(text)):
-	if 'great' in text[i]:
-		if 'than' in text[i+1]:
-			if i+3 < len(text) and 'equal' in text[i+3]:
-				ans = ans + "gte"
-				break
-				i = i+5
-			else:
-				ans = ans + "gt"
-				break
-				i = i+3
-	elif 'less' in text[i]:
-		if 'than' in text[i+1]:
-			if i+3 < len(text) and 'equal' in text[i+3]:
-				ans = ans + "lte"
-				break
-				i = i+5
-			else:
-				ans = ans + "lt"
-				break
-				i = i+3
-	elif 'equal' in text[i]:
-		ans = ans + "eq"
+from watson import watson_run
 
-print(ans)
--- watson.py
+++ watson.py
@@ -1,56 +1,55 @@
-import pyaudio
-import wave
+def startrec():
+	import pyaudio
+	import wave
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 2
+	RATE = 44100
+	RECORD_SECONDS = 5
+	WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+	p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
 
-print("* recording")
+	print("* recording")
 
-frames = []
+	frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
 
-print("* done recording")
+	print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
 
-
-import requests
-import json
-import os
-
-url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=false&max_alternatives=0"
-headers = {"Content-Type": "audio/wav"}
-# payload = {'data-binary':"@~/Downloads/watson/audio-file.flac"}
-# r = requests.post("https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&max_alternatives=3", data={'header': "Content-Type: audio/flac", 'data-binary': '~/Downloads/watson/audio-file.flac'}, auth=("b2aca159-86a0-4afa-9b22-4b720ee1957f", "vTSl6VKjZMPA"))
-user = "b2aca159-86a0-4afa-9b22-4b720ee1957f"
-passwd = "vTSl6VKjZMPA"
-audio_file = os.path.abspath('') + '/output.wav'
-audio = open(audio_file, 'rb')
-r = requests.post(url, data=audio, headers=headers, auth=(user, passwd))
-# print(r.text)
-trans = json.loads(r.text)
-print(trans["results"][0]['alternatives'][0]['transcript'])
+def watson_run(usr, passd):
+	startrec()
+	url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=false&max_alternatives=0"
+	headers = {"Content-Type": "audio/wav"}
+	# payload = {'data-binary':"@~/Downloads/watson/audio-file.flac"}
+	# r = requests.post("https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&max_alternatives=3", data={'header': "Content-Type: audio/flac", 'data-binary': '~/Downloads/watson/audio-file.flac'}, auth=("b2aca159-86a0-4afa-9b22-4b720ee1957f", "vTSl6VKjZMPA"))
+	user = usr
+	passwd = passd
+	audio_file = os.path.abspath('') + '/output.wav'
+	audio = open(audio_file, 'rb')
+	r = requests.post(url, data=audio, headers=headers, auth=(user, passwd))
+	# print(r.text)
+	trans = json.loads(r.text)
+	returned = trans["results"][0]['alternatives'][0]['transcript']
+	return returned
